c5d24c74f465f965a8db08a429f7e2a3fd604b38
lizewei <li.zewei@xyb2b.com>
2023-04-24 12:46:58 +0800

    fix：添加明细不同会员价格字段

--- litemall-db/sql/upgrade/20230423-litemll_goods-DDL.sql
+++ litemall-db/sql/upgrade/20230423-litemll_goods-DDL.sql
@@ -2,3 +2,11 @@ alter table `litemall_goods`
 add column `tag_price` decimal(10,2) DEFAULT '100000.00' COMMENT '吊牌价格';
 alter table `litemall_goods`
 add column `wholesale_price` decimal(10,2) DEFAULT '100000.00' COMMENT '批发价格';
+
+
+alter table `litemall_order_goods`
+add column `tag_price` decimal(10,2) DEFAULT '100000.00' COMMENT '吊牌价格';
+alter table `litemall_order_goods`
+add column `retail_price` decimal(10,2) DEFAULT '100000.00' COMMENT '零售价格';
+alter table `litemall_order_goods`
+add column `wholesale_price` decimal(10,2) DEFAULT '100000.00' COMMENT '批发价格';
