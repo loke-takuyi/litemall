f50ad4aaab3b78ad074bff45d045088d65573bdc
lizewei <li.zewei@xyb2b.com>
2023-04-28 15:52:57 +0800

    fix：不同等级价格

--- litemall-db/sql/upgrade/20230423-litemll_goods-DDL.sql
+++ litemall-db/sql/upgrade/20230423-litemll_goods-DDL.sql
@@ -4,9 +4,9 @@ alter table `litemall_goods`
 add column `wholesale_price` decimal(10,2) DEFAULT '100000.00' COMMENT '批发价格';
 
 
-alter table `litemall_order_goods`
+alter table `litemall_goods_product`
 add column `tag_price` decimal(10,2) DEFAULT '100000.00' COMMENT '吊牌价格';
-alter table `litemall_order_goods`
+alter table `litemall_goods_product`
 add column `retail_price` decimal(10,2) DEFAULT '100000.00' COMMENT '零售价格';
-alter table `litemall_order_goods`
+alter table `litemall_goods_product`
 add column `wholesale_price` decimal(10,2) DEFAULT '100000.00' COMMENT '批发价格';
